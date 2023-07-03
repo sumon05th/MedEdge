b4be65dcf46e61f1a82379e5ef085c9c689b0c38
Sumon Kumar Das <sumonkumardas11th@gmail.com>
2023-07-03 10:43:50 +0530

    report and lab profile design done

--- components/LabProfile.js
+++ components/LabProfile.js
@@ -1,3 +1,142 @@
+// import React, { useEffect, useState } from "react";
+// import { useSession, signOut, getSession } from "next-auth/react";
+// import axios from "axios";
+// import Router, { useRouter } from "next/router";
+// function LabProfile() {
+//   const router = useRouter();
+//   const query = router.query;
+//   const [profile, setProfile] = useState();
+//   const { data: session } = useSession();
+//   const emailt = session.user.email;
+
+//   // const [age, setAge] = useState();
+//   const [address, setAddress] = useState();
+//   // const [bloodgroup, setBloodgroup] = useState();
+//   const [pincode, setPincode] = useState();
+//   // const [gender, setGender] = useState();
+
+//   const fetchData = () => {
+//     const url = `http://localhost:3000/api/getlabprofile/?email=${emailt}`;
+//     return axios.get(url).then((response) => {
+//       setProfile(response.data);
+//       // setAge(response.data.age)
+//       setAddress(response.data.address);
+//       // setBloodgroup(response.data.bloodgroup)
+//       setPincode(response.data.pincode);
+//       // setGender(response.data.gender)
+//     });
+//   };
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
+//   const updateProfile = async (e) => {
+//     e.preventDefault();
+//     const email = session.user.email;
+//     const res = await fetch(`http://localhost:3000/api/updatelabprofile/`, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({ email, address, pincode }),
+//     });
+//     Router.reload();
+//   };
+
+//   return (
+//     <div>
+//       <div>{profile?.name}</div>
+//       {/* <div>{profile?.lastname}</div> */}
+//       <div>{profile?.email}</div>
+//       <div>{profile?.phone}</div>
+//       <div>{profile?.username}</div>
+//       {/* <label>
+//       Age
+//       <input
+//         name="age"
+//         type="text"
+//         value={age}
+//         onChange={(e) => setAge(e.target.value)}
+//       />
+//     </label> */}
+//       <label>
+//         Pincode
+//         <input
+//           name="pincode"
+//           type="text"
+//           value={pincode}
+//           onChange={(e) => setPincode(e.target.value)}
+//         />
+//       </label>
+//       <label>
+//         Address
+//         <input
+//           name="address"
+//           type="text"
+//           value={address}
+//           onChange={(e) => setAddress(e.target.value)}
+//         />
+//       </label>
+//       {/* <label>
+//       Blood Group
+//       <input
+//         name="bloodgroup"
+//         type="text"
+//         value={bloodgroup}
+//         onChange={(e) => setBloodgroup(e.target.value)}
+//       />
+//     </label> */}
+//       {/* <label>
+//       Gender
+//       <input
+//         name="gender"
+//         type="text"
+//         value={gender}
+//         onChange={(e) => setGender(e.target.value)}
+//       />
+//     </label> */}
+//       <div>
+//         <button
+//           className="h-10 w-20 border-2 font-bold bg-red-500  hover:bg-red-700 hover:text-white border-gray-500 rounded-md"
+//           onClick={() =>
+//             signOut({
+//               callbackUrl: `${window.location.origin}`,
+//             })
+//           }
+//         >
+//           Sign out
+//         </button>
+//       </div>
+
+//       <button type="submit" onClick={(e) => updateProfile(e)}>
+//         Submit
+//       </button>
+
+//       <div>
+//         <button className="h-10 w-20 border-2 font-bold hover:bg-green-900 hover:text-white bg-green-400 mr-40  border-gray-500 rounded-md">
+//           <a href="http://localhost:3000/report">Upload Lab Report: </a>
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default LabProfile;
+// // export async function getServerSideProps() {
+// //   const session = await getSession(context);
+// //   const email = session.user.email;
+// //   const profile = await fetch(
+// //     `http://localhost:3000/api/getlabprofile/?email=${email}`
+// //   ).then((res) => res.json());
+// //   return {
+// //     props: {
+// //       session,
+// //       check,
+// //       profile,
+// //       email,
+// //     },
+// //   };
+// // }
+
 import React, { useEffect, useState } from "react";
 import { useSession, signOut, getSession } from "next-auth/react";
 import axios from "axios";
@@ -43,13 +182,26 @@ function LabProfile() {
   };
 
   return (
-    <div>
-      <div>{profile?.name}</div>
-      {/* <div>{profile?.lastname}</div> */}
-      <div>{profile?.email}</div>
-      <div>{profile?.phone}</div>
-      <div>{profile?.username}</div>
-      {/* <label>
+    <div className="bg-red-200 m-3 mb-4 pt-10 grid justify-center items-center">
+      <div>
+        <div className=" mt-2 font-semibold font-mono ml-2 text-lg ">
+          <span className="font-semibold font-mono "> Name :</span>
+          {profile?.name}
+        </div>
+        {/* <div>{profile?.lastname}</div> */}
+        <div className="  mt-2 font-semibold font-mono ml-2 text-lg ">
+          <span className="font-semibold font-mono"> Email :</span>
+          {profile?.email}
+        </div>
+        <div className=" mt-2 font-semibold font-mono ml-2 text-lg">
+          <span className="font-semibold font-mono"> Contact :</span>
+          {profile?.phone}
+        </div>
+        <div className=" mb-4 mt-2 font-semibold font-mono ml-2 text-lg">
+          <span className="font-semibold font-mono"> Username :</span>
+          {profile?.username}
+        </div>
+        {/* <label>
       Age
       <input
         name="age"
@@ -58,25 +210,25 @@ function LabProfile() {
         onChange={(e) => setAge(e.target.value)}
       />
     </label> */}
-      <label>
-        Pincode
-        <input
-          name="pincode"
-          type="text"
-          value={pincode}
-          onChange={(e) => setPincode(e.target.value)}
-        />
-      </label>
-      <label>
-        Address
-        <input
-          name="address"
-          type="text"
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
-        />
-      </label>
-      {/* <label>
+        <label className="mt-10 font-semibold font-mono ml-2 pt-3 text-lg">
+          Pincode
+          <input
+            name="pincode"
+            type="text"
+            value={pincode}
+            onChange={(e) => setPincode(e.target.value)}
+          />
+        </label>
+        <label className="mt-2 pb-10 pt-3 font-semibold flex font-mono ml-2 text-lg">
+          Address
+          <input
+            name="address"
+            type="text"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+        </label>
+        {/* <label>
       Blood Group
       <input
         name="bloodgroup"
@@ -85,7 +237,7 @@ function LabProfile() {
         onChange={(e) => setBloodgroup(e.target.value)}
       />
     </label> */}
-      {/* <label>
+        {/* <label>
       Gender
       <input
         name="gender"
@@ -94,9 +246,23 @@ function LabProfile() {
         onChange={(e) => setGender(e.target.value)}
       />
     </label> */}
+      </div>
+
       <div>
+        <button className="space-y-4 pb-3 h-10 w-40 border-1 font-bold hover:bg-green-900 hover:text-white bg-green-400 mr-40  border-gray-500 rounded-md">
+          <a href="http://localhost:3000/report">Upload Lab Report: </a>
+        </button>
+      </div>
+
+      <div className="space-y-4 pt-3 pb-3 h-10 w-20 border-2 font-bold  hover:bg-blue-900 hover:text-white bg-blue-300 mr-40  border-gray-500 rounded-md">
+        <button type="submit" onClick={(e) => updateProfile(e)}>
+          Submit
+        </button>
+      </div>
+
+      <div className="bg-red-200 mt-6">
         <button
-          className="h-10 w-20 border-2 font-bold bg-red-500  hover:bg-red-700 hover:text-white border-gray-500 rounded-md"
+          className="space-y-4 h-10 w-20 border-2 font-bold bg-red-500  hover:bg-red-700 hover:text-white border-gray-500 rounded-md"
           onClick={() =>
             signOut({
               callbackUrl: `${window.location.origin}`,
@@ -104,16 +270,6 @@ function LabProfile() {
           }
         >
           Sign out
-        </button>
-      </div>
-
-      <button type="submit" onClick={(e) => updateProfile(e)}>
-        Submit
-      </button>
-
-      <div>
-        <button className="h-10 w-20 border-2 font-bold hover:bg-green-900 hover:text-white bg-green-400 mr-40  border-gray-500 rounded-md">
-          <a href="http://localhost:3000/report">Upload Lab Report: </a>
         </button>
       </div>
     </div>
--- pages/report.js
+++ pages/report.js
@@ -1,3 +1,105 @@
+// import React, { useState } from "react";
+// import { useSession, getSession } from "next-auth/react";
+
+// export default function report() {
+//   const { data: session } = useSession();
+//   if (session.user.role !== "lab") {
+//     return (
+//       <div>
+//         <h1>Access Denied</h1>
+//       </div>
+//     );
+//   }
+//   console.log(session.user);
+//   const [message, setMessage] = useState("");
+//   const [username, setUsername] = useState("");
+//   const [image, setImage] = useState(null);
+
+//   const handleUsernameChange = (e) => {
+//     setUsername(e.target.value);
+//   };
+
+//   const handleImageChange = (e) => {
+//     setImage(e.target.files[0]);
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     // Upload image to Cloudinary
+//     const formData = new FormData();
+//     formData.append("file", image);
+//     formData.append("upload_preset", "reportdata"); // Replace with your Cloudinary upload preset
+
+//     const response = await fetch(
+//       "https://api.cloudinary.com/v1_1/dvefqwjbl/image/upload",
+//       {
+//         method: "POST",
+//         body: formData,
+//       }
+//     );
+
+//     const data = await response.json();
+//     const imageUrl = data.secure_url; // Get the uploaded image URL
+
+//     // Save the URL and username to MongoDB
+//     const patientData = {
+//       username,
+//       imageUrl,
+//       labName: session.user.name,
+//     };
+
+//     const saveResponse = await fetch("/api/savereportdata", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(patientData),
+//     });
+//     let datat = await saveResponse.json();
+//     if (datat.message) {
+//       setMessage(datat.message);
+//     }
+//     if (datat.message === "Patient data saved successfully.") {
+//       window.location.reload();
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <form onSubmit={handleSubmit}>
+//         <div>
+//           <label>Username:</label>
+//           <input type="text" value={username} onChange={handleUsernameChange} />
+//         </div>
+//         <div>
+//           <label>Image:</label>
+//           <input type="file" accept="image/*" onChange={handleImageChange} />
+//         </div>
+//         <button type="submit">Submit</button>
+//       </form>
+
+//       <p>{message}</p>
+
+//       <div>
+//         <button>
+//           <a href="http://localhost:3000/profile">Go Back to Lab</a>
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export async function getServerSideProps(context) {
+//   const session = await getSession(context);
+
+//   return {
+//     props: {
+//       session,
+//     },
+//   };
+// }
+
 import React, { useState } from "react";
 import { useSession, getSession } from "next-auth/react";
 
@@ -66,22 +168,25 @@ export default function report() {
   };
 
   return (
-    <div>
+    <div className="bg-green-200 grid justify-center items-center">
       <form onSubmit={handleSubmit}>
-        <div>
+        <div className="pt-3 pb-3 mt-20 font-semibold font-mono ml-2 text-lg">
           <label>Username:</label>
           <input type="text" value={username} onChange={handleUsernameChange} />
         </div>
-        <div>
+        <div className="pt-3 pb-3 mt-20 font-semibold font-mono ml-2 text-lg">
           <label>Image:</label>
           <input type="file" accept="image/*" onChange={handleImageChange} />
         </div>
-        <button type="submit">Submit</button>
+
+        <div className=" mt-4 pb-3 mt-10 h-10 w-40 ml-2 border-1 font-bold hover:bg-green-900 hover:text-white bg-green-400 mr-40  border-gray-500 rounded-md">
+          <button type="submit">Submit</button>
+        </div>
       </form>
 
       <p>{message}</p>
 
-      <div>
+      <div className="mt-20 mb-10 h-10 w-40 ml-2 border-1 font-bold hover:bg-green-900 hover:text-white bg-blue-400 mr-40  border-gray-500 rounded-md ">
         <button>
           <a href="http://localhost:3000/profile">Go Back to Lab</a>
         </button>
